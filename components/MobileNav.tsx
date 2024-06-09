fa8d01513eb10cbc5498c492394972c0a582e5ec
NuwanSD <sagaradnuwan@gmail.com>
2024-06-09 16:28:51 +0530

    first commit

---
+++ components/MobileNav.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MobileNav = () => {
+  return <div className=" text-white-1">MobileNav</div>;
+};
+
+export default MobileNav;
---
+++ components/RightSidebar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RightSidebar = () => {
+  return <div className="text-white-1">RightSidebar</div>;
+};
+
+export default RightSidebar;
